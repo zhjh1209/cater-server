e5eeeacafd3512a39d62832765c32b66be724792
Junhua <zhjh1209@gmail.com>
2018-07-20 10:41:08 +0800

    add mongodb credential

--- src/main/java/com/mokylin/cater_server/config/MongoConfig.java
+++ src/main/java/com/mokylin/cater_server/config/MongoConfig.java
@@ -9,10 +9,29 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @Configuration
 @EnableMongoRepositories(basePackages = {"com.mokylin.cater_server"})
 public class MongoConfig extends AbstractMongoConfiguration {
+    @Value("${mongo.servers}")
+    private String serversStr;
+    @Value("${mongo.username}")
+    private String username;
+    @Value("${mongo.password}")
+    private String password;
 
     @Override
     public MongoClient mongoClient() {
-        MongoClient client = new MongoClient();
+        String[] strArr = serversStr.split(",");
+        List<ServerAddress> servers = new ArrayList<>(strArr.length);
+        for (String str : strArr) {
+            servers.add(new ServerAddress(str));
+        }
+        MongoClient client;
+        if (!StringUtils.isEmpty(username) && !StringUtils.isEmpty(password)) {
+            MongoCredential credential = MongoCredential
+                    .createCredential(username, getDatabaseName(), password.toCharArray());
+            // 密码验证
+            client = new MongoClient(servers, credential, new Builder().build());
+        } else {
+            client = new MongoClient(servers);
+        }
         return client;
     }
 
